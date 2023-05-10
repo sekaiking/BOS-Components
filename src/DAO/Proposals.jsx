bf4398e30476816cad5e19084a997bd6bf09c340
wiwky <wiwky@proton.me>
2023-05-10 11:30:49 +0300

    Implement `InfiniteScroll`

--- src/DAO/Proposals.jsx
+++ src/DAO/Proposals.jsx
@@ -1,21 +1,43 @@
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
-const limit = props.limit ?? 30; // Number of most recent proposals to fetch
-
-const lastProposalId = Near.view(daoId, "get_last_proposal_id");
-const fromIndex = Math.max(0, lastProposalId - limit + 1); // Ensures fromIndex is never less than 0
-
-const proposals = Near.view(daoId, "get_proposals", {
-  from_index: fromIndex,
-  limit: limit,
-});
+const proposalsPerPage = props.proposalsPerPage ?? 5; // Number of proposals to fetch at a time
 
 State.init({
   daoId,
+  proposals: [],
+  lastProposalId: null, // To keep track of the last loaded proposal
+  hasMore: true, // Boolean to know if there are more proposals to load
 });
 
-const onChangeDAO = (daoId) => {
+const loadProposals = () => {
+  const lastProposalId =
+    state.lastProposalId !== null
+      ? state.lastProposalId
+      : Near.view(daoId, "get_last_proposal_id");
+  if (lastProposalId === null) return;
+
+  const fromIndex = Math.max(0, lastProposalId - proposalsPerPage + 1); // Ensures fromIndex is never less than 0
+  const limit = fromIndex === 0 ? lastProposalId + 1 : proposalsPerPage; // Ensure we don't fetch the same proposals twice if fromIndex is 0
+
+  const newProposals = Near.view(daoId, "get_proposals", {
+    from_index: fromIndex,
+    limit: limit,
+  });
+  if (newProposals === null) return;
+
   State.update({
-    daoId,
+    ...state,
+    hasMore: fromIndex > 0,
+    proposals: [...state.proposals, ...newProposals.reverse()],
+    lastProposalId: fromIndex - 1,
+  });
+};
+
+const onChangeDAO = (newDaoId) => {
+  State.update({
+    daoId: newDaoId,
+    proposals: [],
+    lastProposalId: null,
+    hasMore: true,
   });
 };
 
@@ -35,16 +57,15 @@ return (
       <hr />
 
       <div>
-        {proposals
-          .slice()
-          .reverse()
-          .map((proposal, i) => (
+        <InfiniteScroll loadMore={loadProposals} hasMore={state.hasMore}>
+          {state.proposals.map((proposal, i) => (
             <Widget
               key={i}
               src="hack.near/widget/DAO.Proposal"
-              props={{ daoId: state.daoId, id: proposals.length - i - 1 }}
+              props={{ daoId: state.daoId, id: proposal.id }}
             />
           ))}
+        </InfiniteScroll>
       </div>
     </div>
   </>
