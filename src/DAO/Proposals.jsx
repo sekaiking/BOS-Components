82702afb797df65f7f666771bbab1738dd832b60
wiwky <wiwky@proton.me>
2023-05-10 10:16:59 +0300

    use `get_last_proposal_id` to filter by latest proposals

--- src/DAO/Proposals.jsx
+++ src/DAO/Proposals.jsx
@@ -1,8 +1,12 @@
 const daoId = props.daoId ?? "multi.sputnik-dao.near";
+const limit = props.limit ?? 30; // Number of most recent proposals to fetch
+
+const lastProposalId = Near.view(daoId, "get_last_proposal_id");
+const fromIndex = Math.max(0, lastProposalId - limit + 1); // Ensures fromIndex is never less than 0
 
 const proposals = Near.view(daoId, "get_proposals", {
-  from_index: 0,
-  limit: 888,
+  from_index: fromIndex,
+  limit: limit,
 });
 
 State.init({
